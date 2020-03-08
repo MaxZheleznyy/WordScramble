b9bc166739103b5e7d980040370bf5b5af050741
Max <maxzheleznyy@gmail.com>
2020-03-07 21:34:07 -0800

    Words file loader implementation

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -27,7 +27,21 @@ struct ContentView: View {
                 }
             }
             .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
         }
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+
+        fatalError("Could not load start.txt from bundle.")
     }
     
     func addNewWord() {
